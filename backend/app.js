9fa8d89a43cd0982c6f1693c71b8e4b0cd8c7e56
lorenzo <lorenzotijerina@gmail.com>
2023-02-18 20:00:40 -0500

    register and login complete

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ const cors = require("cors");
 const { isProduction } = require("./config/keys");
 
 require("./models/User");
-
+require("./config/passport"); // <-- ADD THIS LINE
+const passport = require("passport"); // <-- ADD THIS LINE
 const usersRouter = require("./routes/api/users");
 const tweetsRouter = require("./routes/api/tweets");
 const csrfRouter = require("./routes/api/csrf");
@@ -24,10 +25,12 @@ app.use(cookieParser());
 
 // ADD THIS SECURITY MIDDLEWARE
 // Security Middleware
+
 if (!isProduction) {
   // Enable CORS only in development because React will be on the React
   // development server (http://localhost:3000). (In production, the Express
   // server will serve the React files statically.)
+  app.use(passport.initialize()); // maybe inside if??
   app.use(cors());
 }
 
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+const passport = require("passport");
 const express = require("express");
 const router = express.Router();
 
@@ -49,6 +50,19 @@ router.post("/register", async (req, res, next) => {
       }
     });
   });
+});
+
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("local", async function (err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error("Invalid credentials");
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
+    }
+    return res.json({ user });
+  })(req, res, next);
 });
 
 module.exports = router;
